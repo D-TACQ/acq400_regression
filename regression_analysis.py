Fixed off by one error in the channel selection.

regression_analysis.py
@@ -28,14 +28,10 @@ def get_data(uuts, args, channels):
             # sample_counter.append()
         else:
             data.append(uut.read_channels((0), -1)[0])
-            print(data)
-            # sample_counter.append(regression_analysis.extract_sample_counter(data[index], regression_analysis.get_agg_chans(uut), uut.nchan()))
             sample_counter.append(extract_sample_counter(data[index], get_agg_chans(uut), uut.nchan()))
             data[index] = data[index].reshape((-1, int(uut.s0.NCHAN)))
-            data[index] = data[index][:,channels[index]]
+            data[index] = data[index][:,np.array(channels[index])-1]
 
-            # print(data[0].shape)
-            # sample_counter.append(regression_analysis.extract_sample_counter(data[index], int(uut.get_ai_channels()), uut.nchan()))
         events.append(uut.get_es_indices(human_readable=1, return_hex_string=1))
 
     return data, events, sample_counter